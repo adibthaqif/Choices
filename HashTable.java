80d1cb043365445aeef24e4c0bdba4fd1dea6022
Don Strong <47014708+dstrong230@users.noreply.github.com>
2019-02-07 17:16:41 -0800

    Update HashTable.java

--- HashTable.java
+++ HashTable.java
@@ -6,8 +6,7 @@ public class HashTable {
 
 //    HashCode() {
 //        if (thisEvent.text.contains("LIFE")) {
-//String hashStr = "LIFE";
-//
+//            String hashStr = "LIFE";
 //        } else if (thisEvent.text.contains("SCHOOL")) {
 //            String hashStr = "CLASS";
 //        } else {
