947efadbe7cf69a1a6e885846e1c169c785ed254
Don Strong <47014708+dstrong230@users.noreply.github.com>
2019-02-07 17:14:37 -0800

    Create HashTable.java
    Just an empty class. It has a potential hashcode/algorithm we can use commented out in it.

---
+++ HashTable.java
@@ -0,0 +1,18 @@
+public class HashTable {
+
+
+
+
+
+//    HashCode() {
+//        if (thisEvent.text.contains("LIFE")) {
+//String hashStr = "LIFE";
+//
+//        } else if (thisEvent.text.contains("SCHOOL")) {
+//            String hashStr = "CLASS";
+//        } else {
+//            String hashStr = "JOB";        //starts with WORK
+//        }
+//        hashStr.length() % array.size();
+//    }
+}
