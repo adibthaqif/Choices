0b0e94565251dcf363338a646290b53ac29ad8b0
Don Strong <47014708+dstrong230@users.noreply.github.com>
2019-02-14 14:56:15 -0800

    Add files via upload

---
+++ HashEntry.java
@@ -0,0 +1,21 @@
+public class HashEntry {
+
+   public HashEntry {
+      
+      HashEntry(int key, Event value) {
+         this.key = key;
+         this.value = value;
+      }
+      
+      public int getKey() {
+         return key;
+      }
+      
+      public Event getValue() {
+         return value;
+      }
+      
+      
+   }
+
+}
--- HashTable.java
+++ HashTable.java
@@ -1,17 +1,40 @@
 public class HashTable {
 
+   public HashTable {
+   
+      private int tableSize = 120;
+      private HashEntry[] hashTable = new HashEntry[tableSize];
+      private int hashAlgorithm, index;
+      }
+   
+   public Event getEvent(int key) {
+      return hashTable[key]
+   }
+   
+   
+   public void put(int key, Event value) {
+      hashAlgorithm = key % tableSize;
+      switch (hashAlgorithm) {
+         case 3:
+            index = 0;
+            while (hashTable[index] != null) {
+               index = index + 1;      
+            }
+            hashTable[index] = value;
+         case 4: 
+            index = 40;
+            while (hashTable[index] != null) {
+               index = index + 1;      
+            }
+            hashTable[index] = value;
+         case 5:
+            index = 80;
+            while (hashTable[index] != null) {
+               index = index + 1;      
+            }
+            hashTable[index] = value;
+      }
+   
+   }
 
-
-
-
-//    HashCode() {
-//        if (thisEvent.text.contains("LIFE")) {
-//            String hashStr = "LIFE";
-//        } else if (thisEvent.text.contains("SCHOOL")) {
-//            String hashStr = "CLASS";
-//        } else {
-//            String hashStr = "JOB";        //starts with WORK
-//        }
-//        hashStr.length() % array.size();
-//    }
 }
