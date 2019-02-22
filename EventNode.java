70c42ef52ca84d6627d2b770916197da52e97eb7
Don Strong <47014708+dstrong230@users.noreply.github.com>
2019-02-21 16:12:04 -0800

    Add files via upload

---
+++ EventNode.java
@@ -0,0 +1,14 @@
+public class EventNode {
+
+    private Event data, lifeEvent, workEvent, schoolEvent;
+
+    public EventNode(Event data, Event lifeEvent, Event workEvent, Event schoolEvent) {
+        this.data = data;
+        this.lifeEvent = lifeEvent;
+        this.workEvent = workEvent;
+        this.schoolEvent = schoolEvent;
+    }
+
+
+
+}
