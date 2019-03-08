42404a071456c397edc80eb0d44e989d6273374c
Don Strong <47014708+dstrong230@users.noreply.github.com>
2019-03-07 17:11:28 -0800

    Update ChoicesManager.java

--- ChoicesManager.java
+++ ChoicesManager.java
@@ -14,22 +14,26 @@ import java.util.*;
 public class ChoicesManager {
     private static Event event;
     private static int key;
-    private static EventHashTable table = new EventHashTable(9);
-
+    private static EventHashTable table = new EventHashTable(23);
+    private int min, max, range;
+    private static EventHeap heap = new EventHeap(6);
+    private static Scanner console = new Scanner(System.in);
+    private static ArrayList<Event> graveyard = new ArrayList<Event>();
 
     public static void main(String[] args)throws FileNotFoundException {
 
-        Scanner console = new Scanner(System.in);
-        System.out.println("Please input file name");
-        String fileName = console.nextLine();
 
-        if(fileName == null) {
-            throw new IllegalArgumentException();
+
+        //Prints Introduction to the screen
+        printIntro();
+        System.out.println("Reading from Choices.txt...");
+        choicesManage("Choices.txt");
+        load();
+        //while (!isGameOver) {
+        play();
+        //}
         }
 
-        choicesManage(fileName);
-
-    }
 
     public static void choicesManage(String file)throws FileNotFoundException {
 
@@ -43,17 +47,59 @@ public class ChoicesManager {
             String eventStr = scanner.nextLine();
             Integer reward = Integer.valueOf(scanner.nextLine());
             Integer punishment = Integer.valueOf(scanner.nextLine());
-            event = new Event(eventStr, reward, punishment);
+            event = new Event(aspect, eventStr, reward, punishment);
             table.put(key, event);
 
         }
 
-        table.printHashTable();
-        System.out.println();
-        System.out.println(table.get(3));
     }
 
+    public static void printIntro() {
+        System.out.println("Welcome to the game of Choices!");
+        System.out.println("Please select one of the following: ");
+        System.out.println("\t Review instructions - i");
+        System.out.println("\t Start a new game - p");
+        System.out.println("\t Save current game - s");
+        System.out.println("\t Quit - q");
+        System.out.println();
+    }
 
+    public static void printInstructions() {
 
+    }
+
+    //Builds the heap with stuff in hashtable
+    public static void load() {
+        Event playEvent = new Event();
+        Scanner console = new Scanner(System.in);
+        //Keep inserting until Heap is full...
+        while (!heap.isFull()) {
+            //If random selected index is null, try again. Else, insert
+            while (playEvent.text == null) {
+                int n = new Random().nextInt(3) + 3;
+                playEvent = table.get(n);
+            }
+            heap.insert(playEvent);
+            playEvent = new Event();
+
+        }
+    }
+
+    public static void play() {
+        Scanner console = new Scanner(System.in);
+        for (int i=0; i <5; i++) {
+            Event current = heap.deleteMin();
+            System.out.println(current.toString());
+            int n = new Random().nextInt(3) + 3;
+            heap.insert(table.get(n));
+            String input = console.nextLine();
+            //changeAspect(input, current);
+            graveyard.add(current);
+        }
+    }
+
+    public static void changeAspect() {
+
+    }
 
 }
